a2b56f44b14ff6310c148bf8e7f6e51ff3996198
David Johnston <dwtj@iastate.edu>
2015-06-08 15:44:57 -0500

    Initial commit.
    Includes a simple java project that implements a linked list for use in
    some initial graph traversal experiments.

---
+++ src/main/java/me/dwtj/List.java
@@ -0,0 +1,38 @@
+package me.dwtj;
+
+public class List<T>
+{
+    private final T head;
+    private final List<T> tail;
+
+    public List(T head) {
+        this.head = head;
+        this.tail = List.<T>nil();
+    }
+
+    public List(T head, List<T> tail) {
+        this.head = head;
+        this.tail = tail;
+    }
+
+    public T head() {
+        return head;
+    }
+
+    public List<T> tail() {
+        return tail;
+    }
+
+    public static <T> List<T> nil() {
+        return null;
+    }
+
+    public static <T> List<T> cat(T[] values)
+    {
+        List<T> xs = List.<T>nil();
+        for (int idx = values.length - 1; idx >= 0; idx--) {
+            xs = new List<T>(values[idx], xs);
+        }
+        return xs;
+    }
+}
---
+++ src/main/java/me/dwtj/Main.java
@@ -0,0 +1,10 @@
+package me.dwtj;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String[] arr = {"foo", "bar", "baz"};
+        List.<String>cat(arr);
+    }
+}
