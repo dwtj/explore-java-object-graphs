3744852c6a432a1486702609f0136d5346fcd422
David Johnston <dwtj@iastate.edu>
2015-06-08 19:16:34 -0500

    Removed all uses of `behaim` from project.

--- src/main/java/me/dwtj/Main.java
+++ src/main/java/me/dwtj/Main.java
@@ -1,7 +1,5 @@
 package me.dwtj;
 
-import com.googlecode.behaim.explorer.Explorer;
-
 public class Main
 {
     public static final int RECURSION_DEPTH = 1000;
@@ -13,7 +11,7 @@ public class Main
         String[] xs_arr = {"foo", "bar", "baz"};
         List<String> xs_list = List.<String>cat(xs_arr);
         System.out.println(">>>>>> xs_list: " + xs_list);
-        Explorer explorer = new Explorer(new ObjectGraphLogger(), RECURSION_DEPTH);
-        explorer.explore(xs_list);
+        //Explorer explorer = new Explorer(new ObjectGraphLogger(), RECURSION_DEPTH);
+        //explorer.explore(xs_list);
     }
 }
--- src/main/java/me/dwtj/ObjectGraphLogger.java
+++ src/main/java/me/dwtj/ObjectGraphLogger.java
@@ -1,24 +1,9 @@
 package me.dwtj;
 
-import java.lang.reflect.Field;
-
-import java.util.Collection;
-import java.util.HashSet;
-
-import com.googlecode.behaim.explorer.VisitationResult;
-import com.googlecode.behaim.explorer.Visitor;
-
-
 /**
- * Taverses an arbitrary Java object graph and prints out some information.
+ * Used to traverse a Java object graph and prints out some information while doing so.
  */
-public class ObjectGraphLogger implements Visitor
+public class ObjectGraphLogger
 {
-	public VisitationResult visit(Object object, Field field)
-    {
-        System.out.println(">>>>>> Object: " + object);
-        System.out.println(">>>>>> Field: " + field);
-        return null;
-    }
-}
 
+}
