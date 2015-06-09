3377c34a06bc650c68bd1285a00bf61a5e8fee96
David Johnston <dwtj@iastate.edu>
2015-06-08 21:48:51 -0500

    `run.sh` now allows multiple log levels through.

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -
 
-mvn exec:java -Dexec.mainClass="me.dwtj.Main" | grep '>>>>>>'
+MESSAGE_PATTERN='^--- |^~~~ |^>>> |^!!! |^[ERROR] '
+
+mvn exec:java -Dexec.mainClass="me.dwtj.Main" | egrep "$MESSAGE_PATTERN"
