c628c295ede3962d99dec3d74c081112d051e105
David Johnston <dwtj@iastate.edu>
2015-06-09 18:26:32 -0500

    Added 3 tests: idempotency, tree, and DAG.

---
+++ src/test/java/me/dwtj/objectgraph/BinaryTree.java
@@ -0,0 +1,20 @@
+package me.dwtj.objectgraph;
+
+public class BinaryTree<T>
+{
+    public final T data;
+    public final BinaryTree<T> left;
+    public final BinaryTree<T> right;
+
+    private BinaryTree(T data, BinaryTree<T> left, BinaryTree<T> right)
+    {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    public static <T> BinaryTree<T> cons(T data, BinaryTree<T> left, BinaryTree<T> right)
+    {
+        return new BinaryTree<T>(data, left, right);
+    }
+}
--- src/test/java/me/dwtj/objectgraph/TestExplorerBasics.java
+++ src/test/java/me/dwtj/objectgraph/TestExplorerBasics.java
@@ -12,22 +12,96 @@ public class TestExplorerBasics
 {
     Explorer default_explorer;
 
+    String[] str_arr;
+    List<String> str_list;
+
+    BinaryTree<String> leaf_a;
+    BinaryTree<String> leaf_b;
+    BinaryTree<String> leaf_c;
+    BinaryTree<String> leaf_d;
+
+    BinaryTree<String> subtree_1;
+    BinaryTree<String> subtree_2;
+    BinaryTree<String> tree;
+
+    BinaryTree<String> dag;
+
     @Before
     public void setUp()
     {
         default_explorer = new Explorer();
+
+        str_arr = new String[3];
+        str_arr[0] = "foo";
+        str_arr[1] = "bar";
+        str_arr[2] = "baz";
+        str_list = List.<String>cat(str_arr);
+
+        leaf_a = BinaryTree.<String>cons("leaf_a", null, null);
+        leaf_b = BinaryTree.<String>cons("leaf_b", null, null);
+        leaf_c = BinaryTree.<String>cons("leaf_c", null, null);
+        leaf_d = BinaryTree.<String>cons("leaf_d", null, null);
+
+        subtree_1 = BinaryTree.<String>cons("subtree_1", leaf_a, leaf_b);
+        subtree_2 = BinaryTree.<String>cons("subtree_2", leaf_c, leaf_d);
+        tree = BinaryTree.<String>cons("tree", subtree_1, subtree_2);
+
+        dag = BinaryTree.<String>cons("dag", subtree_1, leaf_a);
+    }
+
+    @Test
+    public void smallStringArray()
+    {
+        final int EXPECTED_COUNT = 4;
+        default_explorer.explore(str_arr);
+        assertVisitCount(default_explorer, str_arr, EXPECTED_COUNT);
     }
 
     @Test
     public void smallStringList()
     {
         final int EXPECTED_COUNT = 6;
+        default_explorer.explore(str_list);
+        assertVisitCount(default_explorer, str_list, EXPECTED_COUNT);
+    }
+    
+    @Test
+    public void stringArrayAndListIdempotency()
+    {
+        // Initially zero.
+        assertVisitCount(default_explorer, null, 0);
 
-        String[] xs_arr = {"foo", "bar", "baz"};
-        List<String> xs_list = List.<String>cat(xs_arr);
-        default_explorer.explore(xs_list);
+        // Explore the array.
+        default_explorer.explore(str_arr);
+        assertVisitCount(default_explorer, str_arr, 4);
 
-        assertVisitCount(default_explorer, xs_list, EXPECTED_COUNT);
+        // Explore a list which shares elements with the array.
+        default_explorer.explore(str_list);
+        assertVisitCount(default_explorer, str_list, 7);
+
+        // Explore that list again, and see no change.
+        default_explorer.explore(str_list);
+        assertVisitCount(default_explorer, str_list, 7);
+        
+        // Explore the array again, and see no change.
+        default_explorer.explore(str_arr);
+        assertVisitCount(default_explorer, str_list, 7);
+    }
+
+    @Test
+    public void smallStringTree()
+    {
+        final int EXPECTED_COUNT = 14;
+        default_explorer.explore(tree);
+        assertVisitCount(default_explorer, tree, EXPECTED_COUNT);
+    }
+
+    @Test
+    public void smallStringDAG()
+    {
+        final int EXPECTED_COUNT = 8;
+        default_explorer.explore(dag);
+        assertVisitCount(default_explorer, dag, EXPECTED_COUNT);
     }
 
     public static void assertVisitCount(Explorer explorer, Object obj, int expected_count)
